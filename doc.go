f112c232809b9f0423efe16fdb76c64cd10dc2cc
Pascal S. de Kloe <pascal@quies.net>
2022-10-06 18:31:23 +0200

    controllers JSON codec

--- doc.go
+++ doc.go
@@ -1,7 +1,57 @@
 package did
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Doc holds the “core properties” of a DID document.
 type Doc struct {
 	Subject             DID                   `json:"id"`
+	Controllers         Set                   `json:"controller,omitempty"`
 	VerificationMethods []*VerificationMethod `json:"verificationMethod,omitempty"`
+}
+
+// Set represents a string, or a set of strings that confrom to the DID syntax.
+type Set []DID
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (set *Set) UnmarshalJSON(bytes []byte) error {
+	switch bytes[0] {
+	case 'n': // null
+		*set = nil
+		return nil
+
+	case '"':
+		if cap(*set) != 0 {
+			*set = (*set)[:1]
+		} else {
+			*set = make(Set, 1)
+		}
+		return (*set)[0].UnmarshalJSON(bytes)
+
+	case '[':
+		var strings []json.RawMessage
+		err := json.Unmarshal(bytes, &strings)
+		if err != nil {
+			return err
+		}
+
+		if cap(*set) >= len(strings) {
+			*set = (*set)[:len(strings)]
+		} else {
+			*set = make(Set, len(strings))
+		}
+
+		for i, s := range strings {
+			err = (*set)[i].UnmarshalJSON([]byte(s))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+
+	default:
+		return fmt.Errorf("DID string or a set of strings not a JSON string or array: %.12q", bytes)
+	}
 }
--- doc_test.go
+++ doc_test.go
@@ -5,19 +5,45 @@ import (
 	"testing"
 )
 
-func TestDocSubjectJSON(t *testing.T) {
-	const sample = `{
+// Example10 is borrowed from the W3C.
+// https://www.w3.org/TR/did-core/#example-10
+const example10 = `{
   "id": "did:example:123456789abcdefghijk"
 }`
 
+// Example11 is borrowed from the W3C, excluding syntax errors.
+// https://www.w3.org/TR/did-core/#example-did-document-with-a-controller-property
+const example11 = `{
+  "@context": "https://www.w3.org/ns/did/v1",
+  "id": "did:example:123456789abcdefghi",
+  "controller": "did:example:bcehfew7h32f32h7af3"
+}`
+
+func TestDocSubjectJSON(t *testing.T) {
 	var doc Doc
-	err := json.Unmarshal([]byte(sample), &doc)
+	err := json.Unmarshal([]byte(example10), &doc)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	const want = "did:example:123456789abcdefghijk"
 	if got := doc.Subject.String(); got != want {
+		t.Errorf("got subject %q, want %q", got, want)
+	}
+}
+
+func TestDocControllersJSON(t *testing.T) {
+	var doc Doc
+	err := json.Unmarshal([]byte(example11), &doc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if n := len(doc.Controllers); n != 1 {
+		t.Fatalf("got %d controllers, want 1", n)
+	}
+	const want = "did:example:bcehfew7h32f32h7af3"
+	if got := doc.Controllers[0].String(); got != want {
 		t.Errorf("got subject %q, want %q", got, want)
 	}
 }
