532469ed6332b8014c5617dffb174ee40249f109
Pascal S. de Kloe <pascal@quies.net>
2022-12-17 13:56:37 +0100

    JSON ommission tag typo

--- document.go
+++ document.go
@@ -223,7 +223,7 @@ type VerificationMethod struct {
 	Controller DID    `json:"controller"` // required
 
 	// A verification method MAY include additional properties.
-	Additional map[string]json.RawMessage `json:",-"`
+	Additional map[string]json.RawMessage `json:"-"`
 }
 
 // AdditionalString returns the value if, and only if the property is present,
@@ -317,7 +317,7 @@ type Service struct {
 
 	// Each service extension MAY include additional properties and MAY
 	// further restrict the properties associated with the extension.
-	Additional map[string]json.RawMessage `json:",-"`
+	Additional map[string]json.RawMessage `json:"-"`
 }
 
 // AdditionalString returns the value if, and only if the property is present,
